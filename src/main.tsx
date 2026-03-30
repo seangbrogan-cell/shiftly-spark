0433b7f8426bffc3e4463450a0107e410c933b50
seangbrogan-cell <seangbrogan@gmail.com>
2026-03-30 23:19:43 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
+
+
 import * as Sentry from "@sentry/react";
 import { BrowserTracing } from "@sentry/tracing";
 
 // Initialize Sentry BEFORE rendering your app
 Sentry.init({
-  dsn: "https://a0dafcb42bb26df1b6c4e294dfca37c3@o4511135694979072.ingest.de.sentry.io/4511135698387024"", // Get this from sentry.io
+  dsn: "https://a0dafcb42bb26df1b6c4e294dfca37c3@o4511135694979072.ingest.de.sentry.io/4511135698387024", // Get this from sentry.io
   integrations: [
     new BrowserTracing(),
     new Sentry.Replay(),
