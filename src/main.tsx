6a050edb5adcd2b100b56b4ee9e8f95a4ac8ffb2
seangbrogan-cell <seangbrogan@gmail.com>
2026-03-30 23:05:35 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { BrowserTracing } from "@sentry/tracing";
 
 // Initialize Sentry BEFORE rendering your app
 Sentry.init({
-  dsn: "https://YOUR_KEY@sentry.io/YOUR_PROJECT_ID", // Get this from sentry.io
+  dsn: "https://a0dafcb42bb26df1b6c4e294dfca37c3@o4511135694979072.ingest.de.sentry.io/4511135698387024"", // Get this from sentry.io
   integrations: [
     new BrowserTracing(),
     new Sentry.Replay(),
